5f4c419306ec939563fa51a44e1b0589ba5719ba
SHMAL <shmal@orsted.com>
2023-01-06 13:10:11 -0600

    Completed importing the FPM file without preprocessing and getting it into the same format as the original code

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ Revised Version of Clay's Code removing labor intensive work and preprocessing o
 
 """
 
+import pandas as pd
+import glob, os
+
+# import FPM file correctly without preprocessing
+
+files = []
+
+# find input directory folder
+for file in os.listdir("./inputs"):
+    if file.endswith(".fpm"):
+        files.append(file)
+
+mydataset2 = pd.read_csv(f"./inputs/{files[0]}", sep='\t', header=9)
+mydataset2 = mydataset2.set_index("Unnamed: 0")
+pass
